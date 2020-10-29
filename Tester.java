469156b290c5298da9754623e3b8ce6928078ea8
Brandon O'Connor <boconnor20@stuy.edu>
2020-10-28 20:14:05 -0400

    Tested Mr.K's case from hw discussion

--- Tester.java
+++ Tester.java
@@ -10,7 +10,7 @@ public class Tester{
     System.out.println(b1.withdraw(51.0));
     System.out.println(b1.toString());*/
 
-    //Steven Lei's tester file with some slight modifications
+    /*//Steven Lei's tester file with some slight modifications
     BankAccount testAcct = new BankAccount(95, "somepassword");
         // to test each case just replace / with /
 
@@ -43,5 +43,41 @@ public class Tester{
         System.out.println(testAcct.getBalance());      // return 3.5
         System.out.println(testAcct.toString());        // return 95    3.5 (the space is a tab)
         //
+        */
+        //Mr. K's test case
+        BankAccount b1 = new BankAccount(1000001, "abc123");
+      BankAccount b2 = new BankAccount(1000231, "abcasdg23");
+
+      System.out.println("Account b1:");
+      b1.deposit(3000.0);
+      b2.deposit(1234.0);
+      System.out.println(b1);
+      System.out.println("Account b2:");
+      System.out.println(b2);
+      System.out.println();
+
+
+      double cashAmount = 300.0;
+      while(cashAmount < 3000){
+        System.out.println("Attempt to move $"+cashAmount+" from the b1 to b2 account:");
+        if(b1.withdraw(cashAmount) ){
+          if(b2.deposit(cashAmount)){
+            System.out.println(b1);
+            System.out.println(b2);
+            System.out.println();
+
+          }else{
+            //This should never happen.
+            //Error message provided to make sure that is the case
+            System.out.println("CRITICAL ERROR! $"+cashAmount +
+                               " withdrawn from Account #" +  b1.getAccountID() +
+                               "Failed to add to Account #"+b2.getAccountID());
+          }
+        }else{
+          System.out.println("Transfer Failed");
+        }
+
+        cashAmount *= 2;
+      }
   }
 }
